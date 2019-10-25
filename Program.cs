6613a7b1f82ca557d2d4b08afba30994f09c848a
Tony <tonycondon42@gmail.com>
2019-10-25 11:52:00 -0500

    adding declartions

--- Program.cs
+++ Program.cs
@@ -6,13 +6,18 @@ Date of completion:
 Please be sure to comment your code - provide a comment for each structure use in 
 program.
  */
-namespace assignment_three
+namespace payroll
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Declarations
+            string employeeName;
+            int deptNum;
+            double hourlyPay, numOfHoursWorked, salary;
+            int EXIT = -1;
+            string[] arrayDepartment = {"Marketing", "Care and Support", "Information Technology", "Sales", "Legal", "Accounting"};
+            double[] arraySalary = {0.0,0.0,0.0,0.0,0.0,0.0};
         }
     }
 }
