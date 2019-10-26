c365729603590522165d83f9faaa840c924670c0
Condon <conmars@dunwoody.edu>
2019-10-25 19:44:36 -0500

    Adding user inputs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 /**
-Name: 
+Name: Tony Condon
 Date of completion:
 
 Please be sure to comment your code - provide a comment for each structure use in 
@@ -12,12 +12,33 @@ namespace payroll
     {
         static void Main(string[] args)
         {
+            //Declarations
             string employeeName;
             int deptNum;
             double hourlyPay, numOfHoursWorked, salary;
             int EXIT = -1;
             string[] arrayDepartment = {"Marketing", "Care and Support", "Information Technology", "Sales", "Legal", "Accounting"};
             double[] arraySalary = {0.0,0.0,0.0,0.0,0.0,0.0};
+
+            //Housekeeping
+            Console.WriteLine("Welcome to The MPLS Dog Boarding HR Payroll Report Program");
+            Console.WriteLine("To get started, please enter your Department Number");
+            deptNum = Convert.ToInt32(Console.ReadLine());
+
+            //Looping structure to continuous iterate
+            while(deptNum != EXIT){
+                //Get EmployeeName
+                Console.WriteLine("Please enter your name");
+                employeeName = Console.ReadLine();
+
+                //Get Hourly Pay Rate
+                Console.WriteLine("Please enter your hourly pay rate");
+                hourlyPay = Convert.ToDouble(Console.ReadLine());
+
+                //Get Number of Hours Worked
+                Console.WriteLine("Please enter your number of hours worked");
+                numOfHoursWorked = Convert.ToDouble(Console.ReadLine());
+            }
         }
     }
 }
